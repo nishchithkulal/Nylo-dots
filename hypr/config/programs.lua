b1facf81e8c81e99a0a0729d5dd303e1d3c915b8
Nishchith Kulal <nishchithkulal.22@gmail.com>
2026-08-15 13:01:07 +0530

    edit programs.lua
    made variable in file global

--- hypr/config/programs.lua
+++ hypr/config/programs.lua
@@ -3,6 +3,7 @@
 ---------------------
 
 -- Set programs that you use
-local terminal    = "alacritty"
-local fileManager = "dolphin"
-local menu        = "hyprlauncher"
+
+terminal = "alacritty"
+fileManager = "dolphin"
+menu = "hyprlauncher"
