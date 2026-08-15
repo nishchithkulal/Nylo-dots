a23906202371f4afb97ebc9bab54e7ca7148af04
Nishchith Kulal <nishchithkulal.22@gmail.com>
2026-08-15 12:59:54 +0530

    add programs.lua
    thise include variable for programs

---
+++ hypr/config/programs.lua
@@ -0,0 +1,8 @@
+---------------------
+---- MY PROGRAMS ----
+---------------------
+
+-- Set programs that you use
+local terminal    = "alacritty"
+local fileManager = "dolphin"
+local menu        = "hyprlauncher"
