a8a770c2f35a6fcf04886a67dfec68cd81401b02
Nishchith Kulal <nishchithkulal.22@gmail.com>
2026-08-15 17:06:03 +0530

    Hyprland programs.lua - added browser and lock

--- hypr/config/programs.lua
+++ hypr/config/programs.lua
@@ -1,9 +1,5 @@
----------------------
----- MY PROGRAMS ----
----------------------
-
--- Set programs that you use
-
 terminal = "alacritty"
 fileManager = "dolphin"
 menu = "hyprlauncher"
+browser = "zen-browser"
+lock = "hyprlock"
