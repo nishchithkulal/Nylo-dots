fcc6c03eaaff35c152aade336b2613cba6cb74dd
Nishchith Kulal <nishchithkulal.22@gmail.com>
2026-08-15 16:54:10 +0530

    Hyprland keybinds.lua - move to next and prev workspace using just keyboard

--- hypr/config/keybinds.lua
+++ hypr/config/keybinds.lua
@@ -11,7 +11,6 @@ hl.bind(mainMod .. " + left",  hl.dsp.focus({ direction = "left" }))
 hl.bind(mainMod .. " + right", hl.dsp.focus({ direction = "right" }))
 hl.bind(mainMod .. " + up",    hl.dsp.focus({ direction = "up" }))
 hl.bind(mainMod .. " + down",  hl.dsp.focus({ direction = "down" }))
-
 hl.bind(mainMod .. " + SHIFT + left",  hl.dsp.window.move({ direction = "l" }))
 hl.bind(mainMod .. " + SHIFT + right", hl.dsp.window.move({ direction = "r" }))
 hl.bind(mainMod .. " + SHIFT + up",    hl.dsp.window.move({ direction = "u" }))
@@ -19,13 +18,15 @@ hl.bind(mainMod .. " + SHIFT + down",  hl.dsp.window.move({ direction = "d" }))
 
 for i = 1, 10 do
     local key = i % 10
-
     hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
     hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
 end
-
 hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
 hl.bind(mainMod .. " + mouse_up",   hl.dsp.focus({ workspace = "e-1" }))
+hl.bind(mainMod .. " + ALT + left", hl.dsp.focus({ workspace = "e-1" }))
+hl.bind(mainMod .. " + ALT + right",   hl.dsp.focus({ workspace = "e+1" }))
+
+
 
 hl.bind(mainMod .. " + X", hl.dsp.window.drag(), { mouse = true })
 hl.bind(mainMod .. " + C", hl.dsp.window.resize(), { mouse = true })
@@ -35,31 +36,26 @@ hl.bind(
     hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
     { locked = true, repeating = true }
 )
-
 hl.bind(
     "XF86AudioLowerVolume",
     hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
     { locked = true, repeating = true }
 )
-
 hl.bind(
     "XF86AudioMute",
     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
     { locked = true, repeating = true }
 )
-
 hl.bind(
     "XF86AudioMicMute",
     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
     { locked = true, repeating = true }
 )
-
 hl.bind(
     "XF86MonBrightnessUp",
     hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),
     { locked = true, repeating = true }
 )
-
 hl.bind(
     "XF86MonBrightnessDown",
     hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),
